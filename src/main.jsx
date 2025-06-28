23b67f9b2786b59c36e671eb955b7aaea24513ae
a-joel <joelvijay319@gmail.com>
2025-06-28 13:16:44 +0530

    Changes Maded

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './App.css'; // or './app.css' based on casing
+import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import {BrowserRouter} from 'react-router-dom';
